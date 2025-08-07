adf27c25ce6c7aaf14ae969b5a9b80796bf7cbb3
SevvyP <longisaac06@gmail.com>
2025-08-06 19:37:17 -0500

    feat: added utility to load google maps api only once

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,16 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+import { getGoogleMapsLoader } from "@/utils/googleMapsLoader";
 
 export default function GoogleMap() {
   const [map, setMap] = useState<google.maps.Map | null>(null);
 
   useEffect(() => {
-    const loader = new Loader({
-      apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-      version: "weekly",
-      libraries: ["core", "marker", "places", "maps"],
-    });
+    const loader = getGoogleMapsLoader();
 
     loader
       .importLibrary("maps")
